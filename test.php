138bc0c082dcdd13f648acabb23793673184f777
waelhamedci <wael.hamed@travis-ci.com>
2025-11-10 08:30:46 -0500

    test

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+// Check if zlib is loaded
+if (!extension_loaded('zlib')) {
+    echo "zlib is not loaded!";
+    exit(1);
+}
+
+// Sample data to compress
+$data = "This is a test string for zlib compression";
+
+// Compress data
+$compressed = gzcompress($data);
+
+// Decompress data
+$decompressed = gzuncompress($compressed);
+
+// Output test result
+if ($decompressed === $data) {
+    echo "zlib is working correctly!";
+} else {
+    echo "zlib test failed.";
+}
